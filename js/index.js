78066e0dc4f84c9f5f7987f789b32606bb45f66c
Kacper Biedka <kacper.biedka.00@gmail.com>
2019-08-24 14:02:37 +0200

    Created a simple example to learn about fog in three.js

--- js/index.js
+++ js/index.js
@@ -5,119 +5,53 @@
 function main() {
   const canvas = document.querySelector("#c");
   const renderer = new THREE.WebGLRenderer({ canvas });
-  renderer.physicallyCorrectLights = true;
 
-  const fov = 45;
+  const fov = 75;
   const aspect = 2; // the canvas default
   const near = 0.1;
-  const far = 100;
+  const far = 5;
   const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-  camera.position.set(0, 10, 20);
-  camera.lookAt(0, 0, 0);
+  camera.position.z = 2;
 
   const scene = new THREE.Scene();
-  scene.background = new THREE.Color("white");
-
-  const loader = new THREE.TextureLoader();
 
   {
-    const planeSize = 40;
-
-    const texture = loader.load(
-      "https://threejsfundamentals.org/threejs/resources/images/checker.png"
-    );
-    texture.wrapS = THREE.RepeatWrapping;
-    texture.wrapT = THREE.RepeatWrapping;
-    texture.magFilter = THREE.NearestFilter;
-    const repeats = planeSize / 2;
-    texture.repeat.set(repeats, repeats);
-
-    const planeGeo = new THREE.PlaneBufferGeometry(planeSize, planeSize);
-    const planeMat = new THREE.MeshBasicMaterial({
-      map: texture,
-      side: THREE.DoubleSide
-    });
-    planeMat.color.setRGB(1.5, 1.5, 1.5);
-    const mesh = new THREE.Mesh(planeGeo, planeMat);
-    mesh.rotation.x = Math.PI * -0.5;
-    scene.add(mesh);
-  }
-
-  const shadowTexture = loader.load(
-    "https://threejsfundamentals.org/threejs/resources/images/roundshadow.png"
-  );
-  const sphereShadowBases = [];
-  {
-    const sphereRadius = 1;
-    const sphereWidthDivisions = 32;
-    const sphereHeightDivisions = 16;
-    const sphereGeo = new THREE.SphereBufferGeometry(
-      sphereRadius,
-      sphereWidthDivisions,
-      sphereHeightDivisions
-    );
-
-    const planeSize = 1;
-    const shadowGeo = new THREE.PlaneBufferGeometry(planeSize, planeSize);
-
-    const numSpheres = 20;
-    for (let i = 0; i < numSpheres; ++i) {
-      // make a base for the shadow and the sphere.
-      // so they move together.
-      const base = new THREE.Object3D();
-      scene.add(base);
-
-      // add the shadow to the base
-      // note: we make a new material for each sphere
-      // so we can set that sphere's material transparency
-      // separately.
-      const shadowMat = new THREE.MeshBasicMaterial({
-        map: shadowTexture,
-        transparent: true, // so we can see the ground
-        depthWrite: false // so we don't have to sort
-      });
-      const shadowMesh = new THREE.Mesh(shadowGeo, shadowMat);
-      shadowMesh.position.y = 0.001; // so we're above the ground slightly
-      shadowMesh.rotation.x = Math.PI * -0.5;
-      const shadowSize = sphereRadius * 4;
-      shadowMesh.scale.set(shadowSize, shadowSize, shadowSize);
-      base.add(shadowMesh);
-
-      // add the sphere to the base
-      const u = i / numSpheres;
-      const sphereMat = new THREE.MeshPhongMaterial();
-      sphereMat.color.setHSL(u, 1, 0.75);
-      const sphereMesh = new THREE.Mesh(sphereGeo, sphereMat);
-      sphereMesh.position.set(0, sphereRadius + 2, 0);
-      base.add(sphereMesh);
-
-      // remember all 3 plus the y position
-      sphereShadowBases.push({
-        base,
-        sphereMesh,
-        shadowMesh,
-        y: sphereMesh.position.y
-      });
-    }
-  }
-
-  {
-    const skyColor = 0xb1e1ff; // light blue
-    const groundColor = 0xb97a20; // brownish orange
-    const intensity = 2;
-    const light = new THREE.HemisphereLight(skyColor, groundColor, intensity);
-    scene.add(light);
+    const near = 1;
+    const far = 2;
+    const color = "lightblue";
+    scene.fog = new THREE.Fog(color, near, far);
+    scene.background = new THREE.Color(color);
   }
 
   {
     const color = 0xffffff;
     const intensity = 1;
     const light = new THREE.DirectionalLight(color, intensity);
-    light.position.set(0, 10, 5);
-    light.target.position.set(-5, 0, 0);
+    light.position.set(-1, 2, 4);
     scene.add(light);
-    scene.add(light.target);
   }
+
+  const boxWidth = 1;
+  const boxHeight = 1;
+  const boxDepth = 1;
+  const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
+
+  function makeInstance(geometry, color, x) {
+    const material = new THREE.MeshPhongMaterial({ color });
+
+    const cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
+
+    cube.position.x = x;
+
+    return cube;
+  }
+
+  const cubes = [
+    makeInstance(geometry, 0x44aa88, 0),
+    makeInstance(geometry, 0x8844aa, -2),
+    makeInstance(geometry, 0xaa8844, 2)
+  ];
 
   function resizeRendererToDisplaySize(renderer) {
     const canvas = renderer.domElement;
@@ -131,35 +65,19 @@ function main() {
   }
 
   function render(time) {
-    time *= 0.001; // convert to seconds
+    time *= 0.001;
 
-    resizeRendererToDisplaySize(renderer);
-
-    {
+    if (resizeRendererToDisplaySize(renderer)) {
       const canvas = renderer.domElement;
       camera.aspect = canvas.clientWidth / canvas.clientHeight;
       camera.updateProjectionMatrix();
     }
 
-    sphereShadowBases.forEach((sphereShadowBase, ndx) => {
-      const { base, sphereMesh, shadowMesh, y } = sphereShadowBase;
-
-      // u is a value that goes from 0 to 1 as we iterate the spheres
-      const u = ndx / sphereShadowBases.length;
-
-      // compute a position for there base. This will move
-      // both the sphere and its shadow
-      const speed = time * 0.2;
-      const angle = speed + u * Math.PI * 2 * (ndx % 1 ? 1 : -1);
-      const radius = Math.sin(speed - ndx) * 10;
-      base.position.set(Math.cos(angle) * radius, 0, Math.sin(angle) * radius);
-
-      // yOff is a value that goes from 0 to 1
-      const yOff = Math.abs(Math.sin(time * 2 + ndx));
-      // move the sphere up and down
-      sphereMesh.position.y = y + THREE.Math.lerp(-2, 2, yOff);
-      // fade the shadow as the sphere goes up
-      shadowMesh.material.opacity = THREE.Math.lerp(1, 0.25, yOff);
+    cubes.forEach((cube, ndx) => {
+      const speed = 1 + ndx * 0.1;
+      const rot = time * speed;
+      cube.rotation.x = rot;
+      cube.rotation.y = rot;
     });
 
     renderer.render(scene, camera);
