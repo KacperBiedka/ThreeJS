d14a7c8e3b2d34e36c270043b765303a54ff8c55
Kacper Biedka <kacper.biedka.00@gmail.com>
2019-08-24 14:25:09 +0200

    Created an example using the Three.js render target

--- js/index.js
+++ js/index.js
@@ -1,73 +1,31 @@
 "use strict";
 
-/* global THREE, dat */
+/* global THREE */
 
 function main() {
   const canvas = document.querySelector("#c");
   const renderer = new THREE.WebGLRenderer({ canvas });
-  const gui = new dat.GUI();
 
-  const fov = 75;
-  const aspect = 2; // the canvas default
-  const near = 0.1;
-  const far = 5;
-  const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-  camera.position.z = 2;
+  const rtWidth = 512;
+  const rtHeight = 512;
+  const renderTarget = new THREE.WebGLRenderTarget(rtWidth, rtHeight);
 
-  const scene = new THREE.Scene();
+  const rtFov = 75;
+  const rtAspect = rtWidth / rtHeight;
+  const rtNear = 0.1;
+  const rtFar = 5;
+  const rtCamera = new THREE.PerspectiveCamera(rtFov, rtAspect, rtNear, rtFar);
+  rtCamera.position.z = 2;
 
-  // We use this class to pass to dat.gui
-  // so when it manipulates near or far
-  // near is never > far and far is never < near
-  // Also when dat.gui maniplates color we'll
-  // update both the fog and background colors.
-  class FogGUIHelper {
-    constructor(fog, backgroundColor) {
-      this.fog = fog;
-      this.backgroundColor = backgroundColor;
-    }
-    get near() {
-      return this.fog.near;
-    }
-    set near(v) {
-      this.fog.near = v;
-      this.fog.far = Math.max(this.fog.far, v);
-    }
-    get far() {
-      return this.fog.far;
-    }
-    set far(v) {
-      this.fog.far = v;
-      this.fog.near = Math.min(this.fog.near, v);
-    }
-    get color() {
-      return `#${this.fog.color.getHexString()}`;
-    }
-    set color(hexString) {
-      this.fog.color.set(hexString);
-      this.backgroundColor.set(hexString);
-    }
-  }
-
-  {
-    const near = 1;
-    const far = 2;
-    const color = "lightblue";
-    scene.fog = new THREE.Fog(color, near, far);
-    scene.background = new THREE.Color(color);
-
-    const fogGUIHelper = new FogGUIHelper(scene.fog, scene.background);
-    gui.add(fogGUIHelper, "near", near, far).listen();
-    gui.add(fogGUIHelper, "far", near, far).listen();
-    gui.addColor(fogGUIHelper, "color");
-  }
+  const rtScene = new THREE.Scene();
+  rtScene.background = new THREE.Color("red");
 
   {
     const color = 0xffffff;
     const intensity = 1;
     const light = new THREE.DirectionalLight(color, intensity);
     light.position.set(-1, 2, 4);
-    scene.add(light);
+    rtScene.add(light);
   }
 
   const boxWidth = 1;
@@ -79,18 +37,41 @@ function main() {
     const material = new THREE.MeshPhongMaterial({ color });
 
     const cube = new THREE.Mesh(geometry, material);
-    scene.add(cube);
+    rtScene.add(cube);
 
     cube.position.x = x;
 
     return cube;
   }
 
-  const cubes = [
+  const rtCubes = [
     makeInstance(geometry, 0x44aa88, 0),
     makeInstance(geometry, 0x8844aa, -2),
     makeInstance(geometry, 0xaa8844, 2)
   ];
+
+  const fov = 75;
+  const aspect = 2; // the canvas default
+  const near = 0.1;
+  const far = 5;
+  const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+  camera.position.z = 2;
+
+  const scene = new THREE.Scene();
+
+  {
+    const color = 0xffffff;
+    const intensity = 1;
+    const light = new THREE.DirectionalLight(color, intensity);
+    light.position.set(-1, 2, 4);
+    scene.add(light);
+  }
+
+  const material = new THREE.MeshPhongMaterial({
+    map: renderTarget.texture
+  });
+  const cube = new THREE.Mesh(geometry, material);
+  scene.add(cube);
 
   function resizeRendererToDisplaySize(renderer) {
     const canvas = renderer.domElement;
@@ -112,13 +93,24 @@ function main() {
       camera.updateProjectionMatrix();
     }
 
-    cubes.forEach((cube, ndx) => {
+    // rotate all the cubes in the render target scene
+    rtCubes.forEach((cube, ndx) => {
       const speed = 1 + ndx * 0.1;
       const rot = time * speed;
       cube.rotation.x = rot;
       cube.rotation.y = rot;
     });
 
+    // draw render target scene to render target
+    renderer.setRenderTarget(renderTarget);
+    renderer.render(rtScene, rtCamera);
+    renderer.setRenderTarget(null);
+
+    // rotate the cube in the scene
+    cube.rotation.x = time;
+    cube.rotation.y = time * 1.1;
+
+    // render the scene to the canvas
     renderer.render(scene, camera);
 
     requestAnimationFrame(render);
