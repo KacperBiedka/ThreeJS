c78503c48663296d35ae069a3e443af8441c1847
KacperBiedka <kacper.biedka.00@gmail.com>
2019-10-01 16:16:04 +0200

    Created the smoke effect with a render target in plane geometry

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,136 +1,118 @@
 import '../styles/index.scss';
+import Stats from 'stats-js';
 
-// global variables
-var renderer;
-var scene;
-var camera;
-var light;
-
-var orbit;
+var camera, scene, renderer,
+    stats, clock, cubeSineDriver,
+    textGeo, textTexture, textMaterial,
+    light, text, smokeTexture, smokeMaterial,
+    smokeGeo, smokeParticles, delta,
+    geometry, material, mesh,
+    mainScene, mainCamera,
+    plane, planeGeometry, planeMaterial;
 
 var canvas = document.querySelector('#c');
+renderer = new THREE.WebGLRenderer({canvas});
+const rtWidth = 512;
+const rtHeight = 512;
+const renderTarget = new THREE.WebGLRenderTarget(rtWidth, rtHeight);
 
-var MAX_P = 800;
-var particles = [];
-var activeParticles = [];
-var ps;
+init();
+animate();
 
 function init() {
+    stats = new Stats();
+    stats.setMode(0);
+    stats.domElement.style.position = 'absolute';
+    stats.domElement.style.left = '0px';
+    stats.domElement.style.top = '0px';
+    document.body.appendChild(stats.domElement);
 
+    clock = new THREE.Clock();
 
-
-    // create a scene, that will hold all our elements such as objects, cameras and lights.
     scene = new THREE.Scene();
+    mainScene = new THREE.Scene();
 
-    // create a camera, which defines where we're looking at.
-    camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
+    camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 10000 );
+    camera.position.z = 1000;
+    scene.add( camera );
+    const fov = 75;
+    const aspect = 2;  // the canvas default
+    const near = 0.1;
+    const far = 5;
+    mainCamera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+    mainCamera.position.z = 2;
+    mainScene.add( mainCamera );
 
-    // create a render, sets the background color and the size
-    renderer = new THREE.WebGLRenderer({canvas: canvas});
-    renderer.setClearColor(0x000000, 1.0);
-    renderer.setSize(window.innerWidth, window.innerHeight);
+    textGeo = new THREE.PlaneGeometry(300,300);
+    THREE.ImageUtils.crossOrigin = ''; //Need this to pull in crossdomain images from AWS
+    textTexture = THREE.ImageUtils.loadTexture('https://s3-us-west-2.amazonaws.com/s.cdpn.io/95637/quickText.png');
+    textMaterial = new THREE.MeshLambertMaterial({color: 0x00ffff, opacity: 1, map: textTexture, transparent: true, blending: THREE.AdditiveBlending});
+    text = new THREE.Mesh(textGeo,textMaterial);
+    text.position.z = 800;
+    scene.add(text);
 
-    light = new THREE.DirectionalLight( 0xffffff, 2 );
-    scene.add( light );
+    light = new THREE.DirectionalLight(0xffffff,0.5);
+    light.position.set(-1,0,1);
+    scene.add(light);
 
-    // position and point the camera to the center of the scene
-    camera.position.x = 10;
-    camera.position.y = 10;
-    camera.position.z = 10;
-    camera.lookAt(scene.position);
-
-    setupParticleSystem();
-    makeParticle(100);
+    smokeTexture = THREE.ImageUtils.loadTexture('https://s3-us-west-2.amazonaws.com/s.cdpn.io/95637/Smoke-Element.png');
+    smokeMaterial = new THREE.MeshLambertMaterial({color: 0xffffff, map: smokeTexture, transparent: true});
+    smokeGeo = new THREE.PlaneGeometry(300, 300);
+    smokeParticles = [];
 
 
-    // call the render function
+    for (var p = 0; p < 25; p++) {
+        var particle = new THREE.Mesh(smokeGeo,smokeMaterial);
+        particle.position.set(Math.random()*500-250,Math.random()*500-250,Math.random()*1000-100);
+        particle.rotation.z = Math.random() * 360;
+        scene.add(particle);
+        smokeParticles.push(particle);
+    }
+
+    {
+        const color = 0xFFFFFF;
+        const intensity = 1;
+        const light = new THREE.DirectionalLight(color, intensity);
+        light.position.set(-1, 2, 4);
+        mainScene.add(light);
+    }
+
+    planeGeometry = new THREE.PlaneGeometry( 2, 2, 128 );
+    const planeTexture = renderTarget.texture;
+    planeMaterial = new THREE.MeshPhongMaterial( {map: planeTexture });
+    plane = new THREE.Mesh( planeGeometry, planeMaterial );
+    mainScene.add(plane);
+
+
+    document.body.appendChild( renderer.domElement );
+
+}
+
+window.scene = scene;
+
+function animate() {
+
+    // note: three.js includes requestAnimationFrame shim
+    stats.begin();
+    delta = clock.getDelta();
+    requestAnimationFrame( animate );
+    evolveSmoke();
     render();
+    stats.end();
 }
 
-function setupParticleSystem() {
-
-    // initially render the particles off screen
-    var geometry = new THREE.Geometry();
-
-    for (var i = 0 ; i < MAX_P ; i++) {
-        var v = new THREE.Vector3(-10000,-10000,-10000);
-        var rnd = Math.random()/4;
-        geometry.colors.push(new THREE.Color(rnd,0,0 ));
-        particles[i] = v;
+function evolveSmoke() {
+    var sp = smokeParticles.length;
+    while(sp--) {
+        smokeParticles[sp].rotation.z += (delta * 0.2);
     }
-
-
-    geometry.vertices = particles;
-
-    var psMat = new THREE.PointsMaterial();
-    psMat.vertexColors = true;
-    psMat.map = THREE.TextureLoader(require("../assets/ps_ball.png"));
-    psMat.blending = THREE.AdditiveBlending;
-    psMat.transparent = true;
-    psMat.opacity = 0.6;
-//        psMat.size = 0.7;
-
-    ps = new THREE.Points(geometry, psMat);
-    ps.sizeAttenuation = true;
-    ps.sortParticles = true;
-
-
-    scene.add(ps);
-}
-
-function makeParticle(pCount) {
-    for (var i = 0 ; i < pCount ; i++) {
-        var particle =  new THREE.Vector3(Math.random() /2,
-            Math.random() /2,
-            Math.random() /2);
-
-//            var particle = new THREE.Vector3(0,0,0);
-        particle.velX = randomRange(-0.04,0.04);
-        particle.velY = randomRange(0.02,0.07);
-        particle.velZ = randomRange(-0.04,0.04);
-        particle.gravity = -0.000002;
-
-        if (activeParticles.length == MAX_P) {
-            activeParticles.shift();
-
-        }
-
-        activeParticles.push(particle);
-    }
-}
-
-function updateParticle(particle) {
-    particle.x+=particle.velX;
-    particle.y+=particle.velY;
-    particle.z+=particle.velZ;
-
-    particle.velY+=particle.gravity;
-}
-
-function randomRange(min,max) {
-    return (Math.random()*max) + min;
 }
 
 function render() {
+    renderer.setRenderTarget(renderTarget);
     renderer.render(scene, camera);
-
-    // update the position of the particles
-
-
-
-    // update the visualisation of the particels
-    for (var i = 0 ; i < activeParticles.length ; i++) {
-        updateParticle(activeParticles[i]);
-        particles[i].set(activeParticles[i].x,activeParticles[i].y,activeParticles[i].z);
-    }
-
-    makeParticle(10);
-
-    ps.geometry.verticesNeedUpdate = true;
-
+    renderer.setRenderTarget(null);
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    renderer.render( mainScene, mainCamera );
     requestAnimationFrame(render);
 }
-
-// calls the init function when the window is done loading.
-window.onload = init;
-
