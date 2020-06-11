23a10592d0a551ab79c84e2326cb1e20b3002ade
KacperBiedka <kacper.biedka.00@gmail.com>
2020-06-11 17:20:18 +0200

    Added basic class for getting github info

---
+++ src/scripts/GithubInfo.js
@@ -0,0 +1,21 @@
+export default class GithubInfo {
+  constructor(user = "KacperBiedka") {
+    this.info = {};
+    this.user = user;
+    this.error = null;
+    this.getUserInfo();
+  }
+  getUserInfo() {
+    fetch(`https://api.github.com/users/${this.user}`)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        this.info = data.body;
+        console.log(this.info);
+      })
+      .catch((error) => {
+        this.error = error;
+        console.log(this.error);
+      });
+  }
+}
